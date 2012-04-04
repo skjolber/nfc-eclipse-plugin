9ceba021ce6d9e53d139dc26c2e5c286ff8f42e0
trs <thomas.skjolberg@gmail.com>
2012-04-04 14:25:34 +0200

    added MIT license header

--- nfc-eclipse-plugin/src/com/antares/nfc/model/FileDialogCellEditor.java
+++ nfc-eclipse-plugin/src/com/antares/nfc/model/FileDialogCellEditor.java
@@ -1,15 +1,30 @@
-package com.antares.nfc.model;
-
-/*******************************************************************************
- * Copyright (c) 2006, 2009 Eric Rizzo and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/***************************************************************************
  *
- * Contributors:
- *     Eric Rizzo - initial implementation
- *******************************************************************************/
+ * This file is part of the NFC Eclipse Plugin project at
+ * http://code.google.com/p/nfc-eclipse-plugin/
+ *
+ * Copyright (C) 2012 by Thomas Rørvik Skjølberg / Antares Gruppen AS.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ****************************************************************************/
+
+package com.antares.nfc.model;
 
 import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.swt.SWT;
@@ -18,11 +33,7 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
 
-/**
- * A CellEditor that is a blending of DialogCellEditor and TextCellEditor. The user can either type
- * directly into the Text or use the button to open a Dialog for editing the cell's value.
- * 
- */
+
 public class FileDialogCellEditor extends CellEditor {
 
 	private Label defaultLabel;
